18356c3a6b63d248ab7dddeef4df48fa8d277041
Altaf-Mahdi <altaf.mahdi@gmail.com>
2014-11-14 05:36:36 +0000

    update build fingerprint

--- eos.mk
+++ eos.mk
@@ -15,7 +15,7 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
 
-#PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:4.4.3/KTU84M/1158763:user/release-keys PRIVATE_BUILD_DESC="hammerhead-#user 4.4.3 KTU84M 1158763 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="hammerhead" BUILD_FINGERPRINT="google/hammerhead/hammerhead:5.0/LRX21Q/1570415:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 5.0 LRX21Q 1570415 release-keys"
 
 # Enable Torch
 #PRODUCT_PACKAGES += Torch
