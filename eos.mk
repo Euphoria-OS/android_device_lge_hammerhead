948c9605a764dbdf614259f05ce8af0ea54aa19f
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-10-16 11:06:31 +0100

    hammerhead: eos bring up
    Change-Id: I0f3c4b95ccceddc84b228e80f4541be92c741bcd

---
+++ eos.mk
@@ -0,0 +1,29 @@
+# Release name
+PRODUCT_RELEASE_NAME := Nexus5
+
+# Inherit device configuration
+$(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/eos/config/common.mk)
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := hammerhead
+PRODUCT_NAME := eos_hammerhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=hammerhead \
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0/MRA58K/2256973:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0 MRA58K 2256973 release-keys"
+
+# TouchControl package.
+#PRODUCT_PACKAGES += \
+#	TouchControl
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/eos/prebuilt/common/bootanimations/BOOTANIMATION-1080x1920.zip:system/media/bootanimation.zip
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,3 +16,4 @@
 
 add_lunch_combo aosp_hammerhead-userdebug
 add_lunch_combo aosp_hammerhead_fp-userdebug
+add_lunch_combo eos_hammerhead-userdebug
